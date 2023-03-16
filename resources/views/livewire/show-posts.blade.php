62e652c7c71d50eb4bcc188fbace2ab8f983624c
Ismael Avila <ismaelg.avilag@gmail.com>
2023-03-15 23:14:56 -0600

    Se agregaron clases de livewire para darle estilo a los datos de los Posts

--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -4,6 +4,9 @@
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
+    
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        {{ $posts }}
+    </div>
 
-    {{ $posts }}
 </div>

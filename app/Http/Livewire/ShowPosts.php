d5b15d973c59457b2e4357c652f31d318c8f5b24
Ismael Avila <ismaelg.avilag@gmail.com>
2023-03-16 11:34:36 -0600

    Se se agregaron dos variables para al componente ShowPosts para que la consultado de los posts se pueda ordenar por orden ASC y DESC. Se modificó también la consulta.

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -9,10 +9,15 @@ class ShowPosts extends Component
 {
 
     public $search;
+    public $sort = 'id';
+    public $direction = 'desc';
 
     public function render()
     {
-        $posts = Post::where('title', 'like', '%' . $this->search . '%')->orwhere('content', 'like', '%' . $this->search . '%')->get();
+        $posts = Post::where('title', 'like', '%' . $this->search . '%')
+                        ->orwhere('content', 'like', '%' . $this->search . '%')
+                        ->orderBy($this->sort, $this->direction)
+                        ->get();
 
         return view('livewire.show-posts', compact('posts'));
     }

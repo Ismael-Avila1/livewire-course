14997fc4c508b4d9a48cc96aeb97b253d989039d
Ismael Avila <ismaelg.avilag@gmail.com>
2023-03-17 12:43:05 -0600

    Se agregaron inputs y botones al modal para agregar registros de Posts.

--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -1,5 +1,37 @@
 <div>
-    <x-danger-button>
+    <x-danger-button wire:click="$set('open', true)">
         Crear nuevo Post
     </x-danger-button>
+
+
+    <x-dialog-modal wire:model="open">
+
+        <x-slot name="title">
+            Crear nuevo post
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="mb-4">
+                <x-label value="Título del post" />
+                <x-input type="text" class="w-full" />
+            </div>
+
+            <div class="mb-4">
+                <x-label value="Contenido del post" />
+                
+                <textarea class="form-control w-full" rows="6"></textarea>
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-secondary-button wire:click="$set('open', false)">
+                Cancelar
+            </x-secondary-button>
+
+        <x-danger-button>
+            Crear Post
+        </x-danger-button>
+        </x-slot>
+
+    </x-dialog-modal>
 </div>

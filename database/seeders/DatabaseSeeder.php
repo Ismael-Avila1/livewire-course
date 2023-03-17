31727b8db0e0aab91335258063eea616a96780e7
Ismael Avila <ismaelg.avilag@gmail.com>
2023-03-17 14:59:29 -0600

    En la migración de la tabla Post se agregó el campo 'image', que guarda ubicación de una imagen asociada a un post. En el PostFactory se crea una imagen random de 640X480 para cada post, la imagen se almacena en 'public/storage/posts'.  En DatabaseSeeder se crea la carpeta donde se van a almacenar las imagenes de los posts.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Contracts\Cache\Store;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,12 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Storage::makeDirectory('public/posts');
 
         \App\Models\Post::factory(100)->create();
     }

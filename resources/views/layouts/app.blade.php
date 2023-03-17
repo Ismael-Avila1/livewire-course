b632d2c79cef064091f05ae3d5520833bf2c19c4
Ismael Avila <ismaelg.avilag@gmail.com>
2023-03-17 13:21:31 -0600

    Se agregó SweetAlert2 para mostar alertas.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}">
